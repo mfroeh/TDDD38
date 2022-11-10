a82dbb60ab969bd3e747eae40ec092aaf9316f1b
mfroeh <mfroeh0@protonmail.com>
2022-11-10 16:16:24 +0100

    Add specialization

---
+++ specialization/fibonacci.cc
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <iostream>
+
+template <int N>
+class Fibonacci {
+    public:
+        static unsigned long long const value{Fibonacci<N-2>::value + Fibonacci<N-1>::value};
+};
+
+template <>
+class Fibonacci<1> {
+    public:
+        static unsigned long long const value{1};
+};
+
+template <>
+class Fibonacci<0> {
+    public:
+        static unsigned long long const value{0};
+};
+
+int main()
+{
+  assert(Fibonacci<0>::value == 0);
+  assert(Fibonacci<1>::value == 1);
+  assert(Fibonacci<5>::value == 5);
+  assert(Fibonacci<10>::value == 55);
+  assert(Fibonacci<12>::value == 144);
+  std::cout << Fibonacci<20>::value;
+}
