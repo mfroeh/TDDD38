1b051739a9efea95c44529bfd6f86a42d8f50c2f
mfroeh <mfroeh0@pm.me>
2023-01-01 21:54:50 +0100

    Add add_pointers

---
+++ mixed-templates/add_pointers.cc
@@ -0,0 +1,33 @@
+#include <type_traits>
+
+namespace details {
+template <typename T, typename U,
+          typename = std::enable_if_t<!std::is_pointer_v<U>>>
+T add_pointers_helper();
+
+// Can't use trailing return type with
+// std::enable_if_t<std::is_pointer_v<U>, details::add_pointers_helper<T*,
+// std::remove_pointer_t<U>>>()
+template <typename T, typename U,
+          typename = std::enable_if_t<std::is_pointer_v<U>>>
+auto add_pointers_helper() {
+  return details::add_pointers_helper<T *, std::remove_pointer_t<U>>();
+}
+} // namespace details
+
+template <typename T, typename U> struct add_pointers {
+  using type = decltype(details::add_pointers_helper<T, U>());
+};
+
+int main() {
+  static_assert(std::is_same_v<add_pointers<int, int>::type, int>,
+                "int + int = int");
+  static_assert(std::is_same_v<add_pointers<int, int *>::type, int *>,
+                "int + int* = int*");
+  static_assert(std::is_same_v<add_pointers<int *, int *>::type, int **>,
+                "int* + int* = int**");
+  static_assert(std::is_same_v<add_pointers<int **, int *>::type, int ***>,
+                "int** + int* = int**");
+  static_assert(std::is_same_v<add_pointers<int **, int ***>::type, int *****>,
+                "int** + int*** = int*****");
+}
