0f7b8d3d72c0329d6cb4d4f5ecb56de1357b7aa1
mfroeh <mfroeh0@pm.me>
2023-01-11 23:04:18 +0100

    Add SFINAE exercise

---
+++ sfinae/priority.cc
@@ -0,0 +1,85 @@
+#include <utility>
+#include <iostream>
+
+template <typename T>
+auto get_priority(T&& t, int, int, int) -> decltype(t.first())
+{
+  return t.first();
+}
+
+template <typename T>
+auto get_priority(T&& t, int, int, long) -> decltype(t.second())
+{
+  return t.second();
+}
+
+template <typename T>
+auto get_priority(T&& t, int, long, long) -> decltype(t.third())
+{
+    return t.third();
+}
+
+template <typename T>
+int get_priority(T&& t, long, long, long) {
+  return 4;
+}
+
+template <typename T>
+int get_priority(T&& t) {
+    return get_priority(std::forward<T>(t), 1, 1, 1);
+}
+
+struct X
+{
+  int first() const { return 1; }
+};
+
+struct Y
+{
+  int second() const { return 2; }
+};
+
+struct Z
+{
+  int third() const { return 3; }
+};
+
+struct XY
+{
+  int first() const { return 1; }
+  int second() const { return 2; }
+};
+
+struct XZ
+{
+  int first() const { return 1; }
+  int third() const { return 3; }
+};
+
+struct YZ
+{
+  int second() const { return 2; }
+  int third() const { return 3; }
+};
+
+struct XYZ
+{
+  int first() const { return 1; }
+  int second() const { return 2; }
+  int third() const { return 3; }
+};
+
+int main()
+{
+  using std::cout;
+  using std::endl;
+  
+  cout << get_priority(X{}) << endl
+       << get_priority(Y{}) << endl
+       << get_priority(Z{}) << endl
+       << get_priority(XY{}) << endl
+       << get_priority(XZ{}) << endl
+       << get_priority(YZ{}) << endl
+       << get_priority(XYZ{}) << endl
+       << get_priority(5) << endl;
+}
